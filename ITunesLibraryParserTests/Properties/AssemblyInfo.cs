756cfe8394054b43f5b569c0d636b2ad6b346829
Kramer Campbell <kramer@kramerc.com>
2023-08-03 07:22:05 -0700

    Revert "Remove usage of System.Runtime.InteropServices (#2)"
    This reverts commit 2224a9d29b62b28e3b8ef9f8aaf33d8e789f4509.

--- ITunesLibraryParserTests/Properties/AssemblyInfo.cs
+++ ITunesLibraryParserTests/Properties/AssemblyInfo.cs
@@ -1,4 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("896bf9fd-3f75-41d6-bbf3-a6208dac1090")]
